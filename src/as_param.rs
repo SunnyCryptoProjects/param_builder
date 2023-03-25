c6730e6d0ed458acdb20f772a29c8c3c330a52ea
user <you@example.com>
2023-03-25 13:20:23 -0700

    More as param impls

--- src/as_param.rs
+++ src/as_param.rs
@@ -4,14 +4,32 @@ pub trait AsParam {
     fn as_param(&self) -> Param;
 }
 
-impl AsParam for String {
-    fn as_param(&self) -> Param {
-        Box::new(self.clone())
+macro_rules! impl_copy_as_param {
+    ($t:ty) => {
+        impl AsParam for $t {
+            fn as_param(&self) -> Param {
+                Box::new(*self)
+            }
+        }
+    };
+    ($($t:ty),*) => {
+        $(impl_copy_as_param!($t);)*
     }
 }
 
-impl AsParam for bool {
-    fn as_param(&self) -> Param {
-        Box::new(*self)
+impl_copy_as_param![bool, u32, i32, i64];
+
+macro_rules! impl_clone_as_param {
+    ($t:ty) => {
+        impl AsParam for $t {
+            fn as_param(&self) -> Param {
+                Box::new(self.clone())
+            }
+        }
+    };
+    ($($t:ty),*) => {
+        $(impl_clone_as_param!($t);)*
     }
 }
+
+impl_clone_as_param![Vec<u8>, String];
